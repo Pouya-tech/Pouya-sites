fb97c24003bfbe9e7f19a5bafe294a8fabe8c8fc
Pouya-code <pouya.irannezhad1384@gmail.com>
2025-07-12 12:02:25 +0430

    Adding the whole Student-Report-Project

---
+++ src/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./*.php', './*.html'], // مسیر فایل‌هات
+  safelist: [
+    'active:scale-90',
+    'active:scale-95',
+    'active:scale-100',
+    'active:scale-105',
+    'active:scale-110',
+    'active:scale-125',
+    'active:scale-150'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
